9a0738ac68e5698668cb6d5832d095038a23b0ec
Novak <gnovak.boskov@gmail.com>
2015-07-04 19:05:51 +0200

    prelude-xml and prelud e-key-chord modules enabled; Switching buffers with M-{arrows} deleted, Prelude  already have that feature binded to S-{arrows}

--- personal/preload/novak-preload.el
+++ personal/preload/novak-preload.el
@@ -2,8 +2,3 @@
 (set-face-attribute 'default nil :height 110)
 ;; line numbers mode globally
 (global-linum-mode t)
-;; switch between windows using META-{left, up, down, right}
-(global-set-key (kbd "M-<left>")  'windmove-left)
-(global-set-key (kbd "M-<right>") 'windmove-right)
-(global-set-key (kbd "M-<up>")    'windmove-up)
-(global-set-key (kbd "M-<down>")  'windmove-down)
--- prelude-modules.el
+++ prelude-modules.el
@@ -36,3 +36,5 @@
 (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 (require 'prelude-yaml)
+(require 'prelude-xml)
+(require 'prelude-key-chord)
