aa748ab75035bd3108a9cbcd3231a3f3ef7e2f78
Novak <gnovak.boskov@gmail.com>
2015-07-17 19:26:03 +0200

    Bookmarks to be saved with each operation which does bookmarking.

--- personal/preload/novak-preload.el
+++ personal/preload/novak-preload.el
@@ -2,3 +2,5 @@
 (set-face-attribute 'default nil :height 110)
 ;; line numbers mode globally
 (global-linum-mode t)
+;; Each command that sets a bookmark to save bookmarks
+(setq bookmark-save-flag 1)
