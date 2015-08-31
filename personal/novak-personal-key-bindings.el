9eb395a11a7f69a503d4ed7269afbb0729d241e3
Novak Boskov <gnovak.boskov@gmail.com>
2015-08-31 15:11:53 +0200

    multiple-cursors.el key bindings

---
+++ personal/novak-personal-key-bindings.el
@@ -0,0 +1,32 @@
+;;; novak-key-bindings.el --- Personal key bindings  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015 Novak Boskov
+
+;; Author: Novak Boskov <gnovak.boskov@gmail.com>
+;; Keywords: keys
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Personal key bindings
+
+;;; Code:
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(provide 'novak-key-bindings)
+;;; novak-key-bindings.el ends here
