64afb4f895e6d30a18ccebd46cecb58cf09d0dc3
Novak Boškov <gnovak.boskov@gmail.com>
2016-11-17 22:59:05 +0100

    No scroll bar and no line numbers

--- personal/preload/novak-preload.el
+++ personal/preload/novak-preload.el
@@ -1,8 +1,10 @@
 ;; face font size
 (set-face-attribute 'default nil :height 120)
 ;; line numbers mode globally
-(global-linum-mode t)
+;; (global-linum-mode t)
 ;; Each command that sets a bookmark to save bookmarks
+;; Vertical scroll bar
+(scroll-bar-mode -1)
 (setq bookmark-save-flag 1)
 
 ;; Set ispell backend
