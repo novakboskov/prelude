29207a4fbdca22c91ec3667fd02bb3e4ca08bbe7
Novak Boškov <gnovak.boskov@gmail.com>
2017-12-28 14:42:56 +0100

    Company Coq added

--- modules/prelude-more-proofgeneral.el
+++ modules/prelude-more-proofgeneral.el
@@ -24,7 +24,11 @@
 
 ;;; Code:
 
+(prelude-require-package 'company-coq)
+
 (load "~/.emacs.d/lisp/PG/generic/proof-site.el")
+
+(add-hook 'coq-mode-hook #'company-coq-mode)
 
 (provide 'prelude-more-proofgeneral)
 ;;; prelude-more-proofgeneral.el ends here
